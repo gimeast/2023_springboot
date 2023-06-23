a549eabf9ac9a9a6299318b2802842b1cf07930c
ua <dudu.gimeast@gmail.com>
2023-06-24 00:16:12 +0900

    bimove 생성

---
+++ springboot_bimove/src/main/java/com/gimeast/springboot_bimove/SpringbootBimoveApplication.java
@@ -0,0 +1,13 @@
+package com.gimeast.springboot_bimove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBimoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBimoveApplication.class, args);
+    }
+
+}
---
+++ springboot_bimove/src/test/java/com/gimeast/springboot_bimove/SpringbootBimoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimeast.springboot_bimove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBimoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
